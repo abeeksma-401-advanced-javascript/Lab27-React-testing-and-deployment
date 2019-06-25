891c0b9004aa0a4c7718e2ebaf5994d1c44e75d5
Alexander <maniacallion@gmail.com>
2019-06-25 15:30:38 -0500

    test do the green thing

--- src/app.test.js
+++ src/app.test.js
@@ -1,1 +1,14 @@
 'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app';
+
+
+describe('The App', () => {
+  it('loads without dying', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App/>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+});
